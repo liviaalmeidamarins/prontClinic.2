ba529711a65ce9c7c586ac98f918f706fc43f433
raiane <raianebarbosa419@gmail.com>
2024-06-25 12:44:38 -0300

    Formulário alterado

--- View/sistema.php
+++ View/sistema.php
@@ -72,7 +72,7 @@ $email = isset($_SESSION['Cli_email']) ? $_SESSION['Cli_email'] : '';
                     <input type="hidden" name="tipo" value="CPF_Modal">
                     <br>
                     <label for="cpf-conferir">CPF</label>
-                    <input type="text" id="cpf-conferir" name="cpf" placeholder="Digite o CPF" required>
+                    <input type="text" id="cpf-conferir" class="input-modal1" name="cpf" placeholder="Digite o CPF" required>
                     <span id="mensagemErroCPF" class="text-danger"></span><br>
                     <input type="button" class="btn btn-primary" style="background-color: #D6E88D; border: transparent;" value="Consultar" onclick="ConsultarCPF()">
                 </div>
@@ -91,23 +91,23 @@ $email = isset($_SESSION['Cli_email']) ? $_SESSION['Cli_email'] : '';
                     <input type="hidden" name="tipo" value="Cadastro">
                     <br>
                     <label for="cpf-cadastro">CPF *</label>
-                    <input type="text" id="cpf-cadastro" name="cpf-cadastro" placeholder="Digite o CPF" required readonly>
+                    <input type="text" id="cpf-cadastro" class="input-modal1" name="cpf-cadastro" placeholder="Digite o CPF" required readonly>
                     
                     <label for="nome-cadastro">Nome *</label>
-                    <input type="text" id="nome" name="nome" placeholder="Digite o nome" required>
+                    <input type="text" id="nome" class="input-modal1" name="nome" placeholder="Digite o nome" required>
                     <span id="mensagemErroNomeCadastro" class="text-danger"></span><br>
 
                     <label for="telefone-cadastro">Telefone</label>
-                    <input type="tel" id="telefone" name="telefone" placeholder="(DDD) XXXXX-XXXX">
+                    <input type="tel" id="telefone" class="input-modal1" name="telefone" placeholder="(DDD) XXXXX-XXXX">
                     
                     <label for="email-cadastro">E-mail</label>
-                    <input type="email" id="email" name="email" placeholder="Digite o e-mail">
+                    <input type="email" id="email" class="input-modal1" name="email" placeholder="Digite o e-mail">
                     
                     <label for="convenio-cadastro">Convênio</label>
-                    <input type="text" id="convenio" name="convenio" placeholder="Digite o convênio">
+                    <input type="text" id="convenio" class="input-modal1" name="convenio" placeholder="Digite o convênio">
                     
                     <label for="data-nascimento-cadastro">Data de nascimento</label>
-                    <input type="date" id="dataNascimento" name="dataNascimento">
+                    <input type="date" id="dataNascimento" class="input-modal1" name="dataNascimento">
                     
                     <input type="button" class="btn btn-primary" style="background-color: #D6E88D; border: transparent;" value="Cadastrar novo paciente" onclick="Cadastrar()">
                 </div>
@@ -125,24 +125,24 @@ $email = isset($_SESSION['Cli_email']) ? $_SESSION['Cli_email'] : '';
                 <div class="formataçao-modal">
                     <input type="hidden" name="tipo" value="Atualizar_Cadastro">
                     <br>
-                    <label for="cpf-atualizar">CPF *</label>
-                    <input type="text" id="cpf-atualizar" name="cpf-atualizar" placeholder="Digite o CPF" required readonly>
+                    <label for="cpf-atualizar" class="labelModal">CPF *</label>
+                    <input type="text" id="cpf-atualizar" class="input-modal1" name="cpf-atualizar" placeholder="Digite o CPF" required readonly>
                     
-                    <label for="nome-atualizar">Nome *</label>
-                    <input type="text" id="nome-atualizar" name="nome-atualizar" placeholder="Digite o nome" required>
-                    <span id="mensagemErro_Atualizar_nome" class="text-danger"></span><br>
+                    <label for="nome-atualizar" class="labelModal">Nome *</label>
+                    <input type="text" id="nome-atualizar" class="input-modal1" name="nome-atualizar" placeholder="Digite o nome" required>
+                    <span id="mensagemErro_Atualizar_nome" class="text-danger"></span>
             
                     <label for="telefone-atualizar">Telefone</label>
-                    <input type="tel" id="telefone-atualizar" name="telefone-atualizar" placeholder="(DDD) XXXXX-XXXX">
+                    <input type="tel" id="telefone-atualizar" class="input-modal1" name="telefone-atualizar" placeholder="(DDD) XXXXX-XXXX">
                     
                     <label for="email-atualizar">E-mail</label>
-                    <input type="email" id="email-atualizar" name="email-atualizar" placeholder="Digite o e-mail">
+                    <input type="email" id="email-atualizar" class="input-modal1" name="email-atualizar" placeholder="Digite o e-mail">
                     
                     <label for="convenio-atualizar">Convênio</label>
-                    <input type="text" id="convenio-atualizar" name="convenio-atualizar" placeholder="Digite o convênio">
+                    <input type="text" id="convenio-atualizar" class="input-modal1" name="convenio-atualizar" placeholder="Digite o convênio">
                     
                     <label for="data-nascimento-atualizar">Data de nascimento</label>
-                    <input type="date" id="dataNascimento-atualizar" name="dataNascimento-atualizar">
+                    <input type="date" id="dataNascimento-atualizar" class="input-modal1" name="dataNascimento-atualizar">
                     <span id="mensagemErroCadastroAtualizado" class="text-success"></span><br>
             
                     <input type="button" class="btn btn-primary" style="background-color: #D6E88D; border: transparent;" value="Atualizar cadastro" onclick="Atualizar_Cadastro()">
@@ -157,17 +157,17 @@ $email = isset($_SESSION['Cli_email']) ? $_SESSION['Cli_email'] : '';
                     <br>
 
                     <label for="cpf-anamnese"></label>
-                    <input type="hidden" id="cpf-anamnese" name="cpf-anamnese" placeholder="Digite o CPF" required readonly>
+                    <input type="hidden" id="cpf-anamnese" class="input-modal1" name="cpf-anamnese" placeholder="Digite o CPF" required readonly>
             
                     <label for="queixa-principal">Queixa principal e história da doença atual? *</label>
-                    <textarea id="queixa-principal" name="queixa-principal" required></textarea><br>
+                    <textarea id="queixa-principal" class="input-modal1" name="queixa-principal" required></textarea><br>
                     <span id="mensagemErroQueixa" class="text-danger"></span><br>
 
                     <label for="uso-medicação">Uso de medicação</label>
-                    <input type="text" id="uso-medicação" name="uso-medicacao" placeholder="Descreva o uso de medicação, se houver"><br>
+                    <input type="text" id="uso-medicação" class="input-modal1" name="uso-medicacao" placeholder="Descreva o uso de medicação, se houver"><br>
             
                     <label for="alergia">Alergia</label>
-                    <input type="text" id="alergia" name="alergia" placeholder="Descreva alergias, se houver"><br>
+                    <input type="text" id="alergia" class="input-modal1" name="alergia" placeholder="Descreva alergias, se houver"><br>
             
                     <label>Possui as seguintes doenças?</label><br>
                     <div>
@@ -184,22 +184,22 @@ $email = isset($_SESSION['Cli_email']) ? $_SESSION['Cli_email'] : '';
                     </div>
             
                     <label for="cirurgia">Passou por alguma cirurgia?</label>
-                    <input type="text" id="cirurgia" name="cirurgia" placeholder="Descreva cirurgias, se houver"><br>
+                    <input type="text" id="cirurgia" class="input-modal1" name="cirurgia" placeholder="Descreva cirurgias, se houver">
             
                     <label for="sangramento">Quando se fere seu sangramento é?</label>
-                    <select id="sangramento" name="sangramento" required>
+                    <select id="sangramento" class="input-modal1" name="sangramento" required>
                         <option value="normal">Normal</option>
                         <option value="excessivo">Excessivo</option>
                     </select><br>
             
                     <label for="cicatrizacao">Cicatrização?</label>
-                    <select id="cicatrizacao" name="cicatrizacao" required>
+                    <select id="cicatrizacao" class="input-modal1" name="cicatrizacao" required>
                         <option value="normal">Normal</option>
                         <option value="complicada">Complicada</option>
                     </select><br>
             
                     <label for="falta-ar">Sente falta de ar?</label>
-                    <select id="falta-ar" name="falta-ar" required>
+                    <select id="falta-ar" class="input-modal1" name="falta-ar" required>
                         <option value="não">Não</option>
                         <option value="raramente">Raramente</option>
                         <option value="frequentemente">Frequentemente</option>
@@ -207,10 +207,10 @@ $email = isset($_SESSION['Cli_email']) ? $_SESSION['Cli_email'] : '';
                     </select><br>
             
                     <label for="gestante">Gestante? (quantas semanas)</label>
-                    <input type="number" id="gestante" name="gestante" value="0"><br>
+                    <input type="number" id="gestante" class="input-modal1" name="gestante" value="0">
             
                     <label for="observacoes">Observações</label>
-                    <textarea id="observacoes" name="observacoes"></textarea><br>
+                    <textarea id="observacoes" class="input-modal1" name="observacoes"></textarea><br>
 
                     <span id="mensagemSucessoAnamnese" class="text-success"></span><br>
                     <input type="button" class="btn btn-primary" style="background-color: #D6E88D; border: transparent;" value="Criar Anamnese" onclick="Criar_Anamnese()">
@@ -224,28 +224,28 @@ $email = isset($_SESSION['Cli_email']) ? $_SESSION['Cli_email'] : '';
                     <input type="hidden" name="tipo" value="SaudeBucal_form">
 
                     <label for="cpf-anamnesecpf-saude-bucal"></label>
-                    <input type="hidden" id="cpf-saude-bucal" name="cpf-saude-bucal" placeholder="Digite o CPF" required readonly>
+                    <input type="hidden" id="cpf-saude-bucal" class="input-modal1" name="cpf-saude-bucal" placeholder="Digite o CPF" required readonly>
 
                     <label for="reacao-anestesia">Reação à anestesia?</label>
-                    <select id="reacao-anestesia" name="reacao-anestesia" required>
+                    <select id="reacao-anestesia" class="input-modal1" name="reacao-anestesia" required>
                         <option value="não">Não</option>
                         <option value="sim">Sim</option>
                     </select><br>
                     
                     <label for="dor-dentes">Dor nos dentes?</label>
-                    <select id="dor-dentes" name="dor-dentes" required>
+                    <select id="dor-dentes" class="input-modal1" name="dor-dentes" required>
                         <option value="não">Não</option>
                         <option value="sim">Sim</option>
                     </select><br>
         
                     <label for="dor-gengiva">Dor na gengiva?</label>
-                    <select id="dor-gengiva" name="dor-gengiva" required>
+                    <select id="dor-gengiva" class="input-modal1" name="dor-gengiva" required>
                         <option value="não">Não</option>
                         <option value="sim">Sim</option>
                     </select><br>
         
                     <label for="sangramento-gengiva">Sangramento na gengiva?</label>
-                    <select id="sangramento-gengiva" name="sangramento-gengiva" required>
+                    <select id="sangramento-gengiva" class="input-modal1" name="sangramento-gengiva" required>
                         <option value="não">Não</option>
                         <option value="raramente">Raramente</option>
                         <option value="frequentemente">Frequentemente</option>
@@ -253,49 +253,49 @@ $email = isset($_SESSION['Cli_email']) ? $_SESSION['Cli_email'] : '';
                     </select><br>
         
                     <label for="gosto-ruim-boca">Gosto ruim na boca?</label>
-                    <select id="gosto-ruim-boca" name="gosto-ruim-boca" required>
+                    <select id="gosto-ruim-boca" class="input-modal1" name="gosto-ruim-boca" required>
                         <option value="não">Não</option>
                         <option value="sim">Sim</option>
                     </select><br>
         
                     <label for="boca-seca">Boca seca?</label>
-                    <select id="boca-seca" name="boca-seca" required>
+                    <select id="boca-seca" class="input-modal1" name="boca-seca" required>
                         <option value="não">Não</option>
                         <option value="sim">Sim</option>
                     </select><br>
         
                     <label for="ranger-dentes">Ranger dos dentes?</label>
-                    <select id="ranger-dentes" name="ranger-dentes" required>
+                    <select id="ranger-dentes" class="input-modal1" name="ranger-dentes" required>
                         <option value="não">Não</option>
                         <option value="sim">Sim</option>
                     </select><br>
         
                     <label for="dor-maxilar">Dor no maxilar?</label>
-                    <select id="dor-maxilar" name="dor-maxilar" required>
+                    <select id="dor-maxilar" class="input-modal1" name="dor-maxilar" required>
                         <option value="não">Não</option>
                         <option value="sim">Sim</option>
                     </select><br>
         
                     <label for="dor-ouvido">Dor no ouvido?</label>
-                    <select id="dor-ouvido" name="dor-ouvido" required>
+                    <select id="dor-ouvido" class="input-modal1" name="dor-ouvido" required>
                         <option value="não">Não</option>
                         <option value="sim">Sim</option>
                     </select><br>
         
                     <label for="ultimo-tratamento">Último tratamento?</label>
-                    <input type="date" id="ultimo-tratamento" name="ultimo-tratamento"><br>
+                    <input type="date" id="ultimo-tratamento" class="input-modal1" name="ultimo-tratamento">
         
                     <label for="fumante">Fumante?</label>
-                    <select id="fumante" name="fumante" required>
+                    <select id="fumante" class="input-modal1" name="fumante" required>
                         <option value="não">Não</option>
                         <option value="sim">Sim</option>
                     </select><br>
         
                     <label for="escova-dentes">Escova dentes? (vezes por dia)</label>
-                    <input type="number" id="escova-dentes" name="escova-dentes" required><br>
+                    <input type="number" id="escova-dentes" class="input-modal1" name="escova-dentes" required>
         
                     <label for="fio-dental">Usa fio dental?</label>
-                    <select id="fio-dental" name="fio-dental" required>
+                    <select id="fio-dental" class="input-modal1" name="fio-dental" required>
                         <option value="não">Não</option>
                         <option value="sim">Sim</option>
                     </select><br>
@@ -323,7 +323,6 @@ $email = isset($_SESSION['Cli_email']) ? $_SESSION['Cli_email'] : '';
                     <input type="text" name="email" id="input-modal3" placeholder="Email cadastrado" value="<?php echo htmlspecialchars($email); ?>">
                     <label for="telefone">Telefone</label>
                     <input type="number" name="telefone" id="input-modal3" placeholder="(ddd) xxxxx-xxxx">
-                    <button class="botao-Enviar" style="font-weight: bolder; color: white;">Salvar</button>
                 </div>
             </form>
 
